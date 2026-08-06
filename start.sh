0e8445154ab4af9c2c95db79716a521791bc06c5
armdix4-sys <armdix4@gmail.com>
2026-08-06 08:14:57 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-set -e
-python -m uvicorn proxy_server:app --host 0.0.0.0 --port ${PORT:-10000}
+#!/usr/bin/env bash
+export PYTHONPATH=$PYTHONPATH:.
+uvicorn proxy_server:app --host 0.0.0.0 --port ${PORT:-8000}
